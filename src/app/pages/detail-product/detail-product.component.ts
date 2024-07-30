9ff9e2950708eaa109953eb3d6ee099a63c7d2c0
triastartya <triastartya@gmail.com>
2024-07-30 19:43:33 +0700

    1

--- src/app/pages/detail-product/detail-product.component.ts
+++ src/app/pages/detail-product/detail-product.component.ts
@@ -1,7 +1,10 @@
-import { Component,OnInit,VERSION } from '@angular/core';
-import { LayoutsComponent } from 'src/app/components/layouts/layouts.component';
-import { FooterComponent } from "../footer/footer.component";
 import { CurrencyPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LayoutsComponent } from 'src/app/components/layouts/layouts.component';
+import { ProdukService } from 'src/app/service/produk/produk.service';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-detail-product',
@@ -16,13 +19,29 @@ export class DetailProductComponent implements OnInit {
   pricePerItem = 799000;
   qty_value: number = 1;
   subtotal: number;
+  id:any
 
-  constructor(private currencyPipe: CurrencyPipe) {
+  detail:any ;
+
+  constructor(
+    private currencyPipe: CurrencyPipe,
+    private produkService: ProdukService,
+    private activedRoute: ActivatedRoute,
+    private domsanitizer: DomSanitizer,
+    private router:Router
+  ) {
     this.subtotal = this.pricePerItem; // Initialize subtotal based on the price of one item
   }
 
   ngOnInit(): void {
-    console.log(this.qty_value);
+    this.id = this.activedRoute.snapshot.params['id'];
+    this.produkService.getdetailproduk(this.id).subscribe(result=>{
+      this.detail = result.data
+      this.pricePerItem = parseInt(result.data.harga)
+      this.subtotal = parseInt(result.data.harga)
+      this.detail.deskripsi_produk = this.domsanitizer.bypassSecurityTrustHtml(this.detail.deskripsi_product);
+      console.log(result.data)
+    }) 
   }
 
   decrement(e: any) {
@@ -37,5 +56,17 @@ export class DetailProductComponent implements OnInit {
     this.qty_value++;
     this.subtotal = this.qty_value * this.pricePerItem;
     return this.currencyPipe.transform(this.subtotal,'IDR');
+  }
+
+  addcart(){
+    let data = {
+      id_customer : 1,
+      id_product : this.id,
+      qty : this.qty_value
+    }
+    this.produkService.addcart(data).subscribe(result=>{
+      console.log(result);
+      this.router.navigateByUrl('shoping-chart')
+    });
   }
 }
--- src/app/service/produk/produk.service.ts
+++ src/app/service/produk/produk.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
+import { HttpOperationService } from '../httpOperation/http-operation.service';
+import { UtilityService } from '../utility/utility.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdukService {
 
-  constructor() { }
+  constructor(private httpOperationService:HttpOperationService,
+    private utilityService:UtilityService,
+    private router:Router
+){}
+  
+  getallproduk():Observable<any>{
+    return this.httpOperationService.getRequest(environment.url+'product/getall');
+  }
+
+  getdetailproduk(id:any):Observable<any>{
+    return this.httpOperationService.getRequest(environment.url+'product/getById/'+id);
+  }
+
+  addcart(data:any):Observable<any>{
+    return this.httpOperationService.onPostRequest(environment.url+'cart/create',data);
+  }
+
 }

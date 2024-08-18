c493aad6a15cac33ae76aef188c466b5199fe5b9
YuliAlfalah <yuliasaalfalah33@gmail.com>
2024-08-18 22:16:37 +0700

    update

--- src/app/pages/profiles/profiles.component.ts
+++ src/app/pages/profiles/profiles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LayoutsComponent } from "../../components/layouts/layouts.component";
+import { UtilityService } from 'src/app/service/utility/utility.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profiles',
@@ -8,6 +10,27 @@ import { LayoutsComponent } from "../../components/layouts/layouts.component";
   standalone:true,
   imports: [LayoutsComponent]
 })
-export class ProfilesComponent {
+export class ProfilesComponent implements OnInit {
+
+  constructor(private utilityService:UtilityService,
+              private router:Router
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.checkIsLogin()
+  }
+
+  checkIsLogin():void{
+    const isLogin = localStorage.getItem('BATIK_')
+    console.log(isLogin)
+    if(isLogin == null){
+      this.utilityService.onShowCustomAlert('warning','Perhatian','Maaf Anda Belum Login')
+      .then(()=>{
+        this.router.navigateByUrl('')
+      })
+    }
+  }
 
 }
